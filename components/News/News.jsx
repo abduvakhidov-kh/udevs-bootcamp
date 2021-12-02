8ca19adced54ef57fd7c0c22e659e1772d0242fb
Khabibullo Abduvakhidov <habibulloabduvahidov03@gmail.com>
2021-12-02 22:28:32 +0500

    fixed errors and add pagination

--- components/News/News.jsx
+++ components/News/News.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NewCard from "../NewsCard/NewCard";
+import PaginationRounded from "../Pagination/Pagination";
 import styles from "./News.module.css";
 
 export default function index() {
@@ -17,6 +18,9 @@ export default function index() {
       <NewCard />
       <NewCard />
       <NewCard />
+      <div className={styles.pagination}>
+        <PaginationRounded />
+      </div>
     </div>
   );
 }
--- pages/[newId].js
+++ pages/[newId].js
@@ -1,10 +1,17 @@
-import { useRouter } from "next/router"
-import styles from "../styles/DetailsPage.module.css"
+// import { useRouter } from "next/router";
+import styles from "../styles/DetailsPage.module.css";
 
-export default function detailsPage () {
-    return (
-        <div className={styles.detailsPage}>
-            
-        </div>
-    )
+function detailsPage() {
+  const router = useRouter();
+
+  const newId = router.query.newId;
+
+  // send request to backend api
+  return (
+    <div className={styles.detailsPage}>
+      <span>fck u</span>
+    </div>
+  );
 }
+
+export default detailsPage;
