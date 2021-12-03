1031388436e7cf8f5ec1a0e4b805fcf26bf1a147
Khabibullo Abduvakhidov <habibulloabduvahidov03@gmail.com>
2021-12-03 14:30:16 +0500

    finally fixed deploy error

--- components/News/News.jsx
+++ components/News/News.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import Link from "next/link";
 import NewCard from "../NewsCard/NewCard";
 import PaginationRounded from "../Pagination/Pagination";
 import styles from "./News.module.css";
 
 export default function index() {
   return (
-    <div className={styles.news}>
-      <Link href="/news/something-l">
-        <NewCard />
-      </Link>
+    <div className={`${styles.news}`}>
+      <NewCard />
       <NewCard />
       <NewCard />
       <NewCard />
